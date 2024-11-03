3568c0251a96a60f41065b20db8d751ebb012a77
Jitesh Kumar Narula <70373993+jiteshnarula44@users.noreply.github.com>
2024-11-03 21:19:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ app = FastAPI()
 # Configuration
 connection_string = 'DefaultEndpointsProtocol=https;AccountName=narula12storage;AccountKey=s8rUHL11ngvXxzJMatsIPT1UKaQsXMw61lKTTb7xA4bM2AawsFIpuf0I4Ty5rwsPpqg4t6IDGe6c+AStCavGIg==;EndpointSuffix=core.windows.net'
 container_name = 'intermediate'
-logic_app_url = 'https://prod-30.northcentralus.logic.azure.com:443/workflows/3b6f71f697e74fa09220c7a5fcc97462/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=brM2VA8ivnivrSz5hBqS8bcfUZgDwOSciIKaipL_4Ys'
+logic_app_url = 'https://prod-12.northcentralus.logic.azure.com:443/workflows/3672412e663142a6a164bb280a34ac6a/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=ntkb7fuoUOOtPG2lviemCr4JBBkhvY6EeD9SjlyJCls'
 
-# Use async1 lifespan context manager to initialize BlobServiceClient
+# Use async lifespan context manager to initialize BlobServiceClient
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     global blob_service_client
@@ -51,17 +51,20 @@ def process_sales_reps():
                     sales_rep_email = row.iloc[1]
                     internal_sales_rep_email = row.iloc[3]
                     
+                    # Extract only the file name without any directory path
+                    only_file_name = file_name.split('/')[-1]  # Modify this line
+
                     data = {
                         'sales_rep_name': sales_rep_name,
                         'sales_rep_email': sales_rep_email,
                         'internal_sales_rep_email': internal_sales_rep_email,
-                        'file_name': file_name  
+                        'file_name': only_file_name  # Use only_file_name here
                     }
 
                     try:
                         response = requests.post(logic_app_url, json=data)
                         if response.status_code == 200:
-                            print(f'Successfully sent data for {sales_rep_name} with file {file_name}')
+                            print(f'Successfully sent data for {sales_rep_name} with file {only_file_name}')
                     except Exception as e:
                         print(f'Error sending data for {sales_rep_name}: {e}')
             
